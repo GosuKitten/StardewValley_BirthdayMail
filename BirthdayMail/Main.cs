bb681e8ec77d95974de72ff212a73f5643dee0b8
Kathryn <KathrynHazuka@hotmail.com>
2016-04-08 02:16:08 -0600

    Removed unnecessary references

--- BirthdayMail/Main.cs
+++ BirthdayMail/Main.cs
@@ -2,14 +2,8 @@
 // Version: 1.1.0
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using StardewModdingAPI;
 using StardewValley;
-using System.Timers;
-using System.Collections;
 
 namespace BirthdayMail
 {
