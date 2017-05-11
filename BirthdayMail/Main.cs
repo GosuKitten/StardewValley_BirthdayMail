6d5831ddd8fae29d9d81e4f53c78d78ad1fd5ce3
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-05-10 22:01:17 -0400

    simplify code using new SMAPI event

--- BirthdayMail/Main.cs
+++ BirthdayMail/Main.cs
@@ -1,5 +1,6 @@
 using System;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
 
 namespace BirthdayMail
@@ -7,73 +8,39 @@ namespace BirthdayMail
     public class Main : Mod
     {
         /*********
-        ** Properties
-        *********/
-        private bool firstUpdate = true;
-
-
-        /*********
         ** Public methods
         *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            // submit to events in StardewModdingAPI
-            StardewModdingAPI.Events.TimeEvents.DayOfMonthChanged += Event_DayOfMonthChanged;
+            TimeEvents.AfterDayStarted += this.TimeEvents_AfterDayStarted;
         }
 
 
         /*********
         ** Private methods
         *********/
-        // runs once per second from the start of the game 
-        // used for first update to solve the mail not being sent on initial load when adding the mod
-        private void Event_OneSecondTick(object sender, EventArgs e)
+        /// <summary>The event called when a new day begins.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void TimeEvents_AfterDayStarted(object sender, EventArgs e)
         {
-            // ...check for birthdays and send mail
-            BirthdayMail();
-            firstUpdate = false;
-            // ...unsubmit from this event for optimization.
-            StardewModdingAPI.Events.GameEvents.OneSecondTick -= Event_OneSecondTick;
+            this.BirthdayMail();
         }
 
-        // runs when the day changes
-        private void Event_DayOfMonthChanged(object sender, EventArgs e)
-        {
-            // if this is the initial update...
-            if (firstUpdate)
-            {
-                // subscribe the Event_OneSecondTick() function to the OneSecondTick event
-                StardewModdingAPI.Events.GameEvents.OneSecondTick += Event_OneSecondTick;
-            }
-            else // otherwise...
-            {
-                // ...check for birthdays and send mail as usual
-                BirthdayMail();
-            }
-
-        }
-
-        // checks for birthdays and sends mail if needed
+        /// <summary>Check if there's a birthday today, and add mail if needed.</summary>
         private void BirthdayMail()
         {
-            // test if today is someone's birthday...
-            if (Utility.getTodaysBirthdayNPC(Game1.currentSeason, Game1.dayOfMonth) != null)
-            {
-                // ...set birthday NPC and their mail item
-                NPC birthdayNPC = Utility.getTodaysBirthdayNPC(Game1.currentSeason, Game1.dayOfMonth);
-                string birthdayMail = birthdayNPC.name + "Birth";
+            // get birthday NPC
+            NPC npc = Utility.getTodaysBirthdayNPC(Game1.currentSeason, Game1.dayOfMonth);
+            if (npc == null || !Game1.player.friendships.ContainsKey(npc.name))
+                return;
 
-                // if the player knows this NPC...
-                if (Game1.player.friendships.ContainsKey(birthdayNPC.name))
-                {
-                    // ...if the mailbox doesn't already have the birthday mail...
-                    if (!Game1.mailbox.Contains(birthdayMail))
-                    {
-                        // ...add the birthday reminder to the mailbox
-                        Game1.mailbox.Enqueue(birthdayMail);
-                    }
-                }
-            }
+            // add reminder to mailbox
+            string mailID = npc.name + "Birth";
+            if (!Game1.mailbox.Contains(mailID))
+                Game1.mailbox.Enqueue(mailID);
         }
     }
 }
